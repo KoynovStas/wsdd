56198af935efd68b18ac7b8b5a99ad777d8b074a
KoynovStas <skojnov@yandex.ru>
2017-03-02 15:06:54 +0300

    add code for wsdd_event_Probe -daemon is work :)

--- src/client_events.c
+++ src/client_events.c
@@ -45,5 +45,30 @@ soap_wsdd_mode wsdd_event_Resolve(struct soap *soap, const char *MessageID, cons
 
 soap_wsdd_mode wsdd_event_Probe(struct soap *soap, const char *MessageID, const char *ReplyTo, const char *Types, const char *Scopes, const char *MatchBy, struct wsdd__ProbeMatchesType *matches)
 {
+    struct wsdd_param_t *wsdd_param = (struct wsdd_param_t *)soap->user;
+
+    if(!wsdd_param)
+        return SOAP_WSDD_ADHOC;
+
+
+    soap_wsdd_init_ProbeMatches(soap, matches);
+
+    soap_wsdd_add_ProbeMatch(soap,
+                             matches,
+                             wsdd_param->endpoint,
+                             wsdd_param->type,
+                             wsdd_param->scope,
+                             NULL,
+                             wsdd_param->xaddr,
+                             wsdd_param->metadata_ver);
+
+    soap_wsdd_ProbeMatches(soap,
+                           NULL,
+                           soap_wsa_rand_uuid(soap) ,
+                           MessageID,
+                           ReplyTo,
+                           matches);
+
+
     return SOAP_WSDD_ADHOC;
 }
